699fc7c2e8a2ae0cb2c3cfe8c72374a1053dc8b1
matchmen <1142229061@qq.com>
2020-09-30 17:21:17 +0800

    测试git命令   master分支

--- src/main/java/com/lqm/web/controller/ExcelController.java
+++ src/main/java/com/lqm/web/controller/ExcelController.java
@@ -57,6 +57,7 @@ public class ExcelController {
         order.setGoods(goodsList);
         goodsList.add(new Goods("玉米", "1212"));
         goodsList.add(new Goods("土豆", "1213"));
+        goodsList.add(new Goods("豆", "1213"));
 
         try {
 
